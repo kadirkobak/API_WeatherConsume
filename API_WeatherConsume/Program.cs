69c8f75409427d56b0b2b38f2b9be8758f32b074
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-18 16:09:39 +0300

    Basic configurations.

--- API_WeatherConsume/Program.cs
+++ API_WeatherConsume/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to API Consume Application");
+Console.WriteLine();
+Console.WriteLine(" ###  Please select a transaction  ###");
+Console.WriteLine(" 1. Get city list");
+Console.WriteLine(" 2. Add a new city");
+Console.WriteLine(" 3. Delete a city");
+Console.WriteLine(" 4. Update a city");
+Console.WriteLine(" 5. Get city by ID");
