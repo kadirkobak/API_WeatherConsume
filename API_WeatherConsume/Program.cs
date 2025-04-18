c37558bc5f041c4ede0793fa41408f3fad2e7424
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-18 17:24:05 +0300

    The first phase of communication with the API has been completed.

--- API_WeatherConsume/Program.cs
+++ API_WeatherConsume/Program.cs
@@ -1,40 +1,64 @@
-﻿Console.WriteLine("Welcome to API Consume Application");
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+
+Console.WriteLine("Welcome to API Consume Application");
 Console.WriteLine();
 Console.WriteLine(" ###  Please select a transaction  ### \n");
 Console.WriteLine(" 1 - Get city list");
-Console.WriteLine(" 2 - Add a new city");
-Console.WriteLine(" 3 - Delete a city");
-Console.WriteLine(" 4 - Update a city");
-Console.WriteLine(" 5 - Get city by ID");
+Console.WriteLine(" 2 - Get city name and weather list");
+Console.WriteLine(" 3 - Add a new city");
+Console.WriteLine(" 4 - Delete a city");
+Console.WriteLine(" 5 - Update a city");
+Console.WriteLine(" 6 - Get city by ID");
 Console.WriteLine();
 
-int choice;
+string choice;
 
 
 Console.WriteLine("Please make your choice: ");
-choice = Convert.ToInt32(Console.ReadLine());
+choice = Console.ReadLine();
 
-if (choice == 1)
+if (choice == "1")
 {
-    Console.WriteLine("Transaction 1");
-} 
-else if (choice == 2)
+    string url = "https://localhost:7222/api/Weathers";
+
+    using (HttpClient client = new HttpClient())
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        string responseBody = await response.Content.ReadAsStringAsync();
+        JArray jArray = JArray.Parse(responseBody);
+
+        foreach (var item in jArray)
+        {
+            string cityName = item["cityName"].ToString();
+            Console.WriteLine($"City Name: {cityName}");
+        }
+    }
+
+}
+else if (choice == "2")
 {
     Console.WriteLine("Transaction 2");
 }
-else if (choice == 3)
+else if (choice == "3")
 {
     Console.WriteLine("Transaction 3");
 }
-else if (choice == 4)
+else if (choice == "4")
 {
     Console.WriteLine("Transaction 4");
 }
-else if (choice == 5)
+else if (choice == "5")
 {
     Console.WriteLine("Transaction 5");
+}
+else if (choice == "6")
+{
+    Console.WriteLine("Transaction 6");
 }
 else
 {
     Console.WriteLine("Invalid choice. Please try again.");
 }
+
+Console.Read();
